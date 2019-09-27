33f3d6b040c063b0187f0f3407fb6bbc9eb3fedb
Qing Wang <chingwang.qw@gmail.com>
2019-09-27 21:22:01 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,13 +1,43 @@
 def nyc_pigeon_organizer(data)
   pigeon_list = {}
   # Do all colours.
-  data[:color].each do |attri_value|
-    
-    end
-  
+  data[:color].each do |color, pigeon_names|
+    pigeon_names.each do |name|
+      if !pigeon_list[name]
+        pigeon_list[name] = {}
+      end
+      if !pigeon_list[name][:color]
+        pigeon_list[name][:color] = []
+      end
+      pigeon_list[name][:color].push(color)
+    end 
+  end
   
   # Do all genders.
+  data[:gender].each do |gender, pigeon_names|
+    pigeon_names.each do |name|
+      if !pigeon_list[name]
+        pigeon_list[name] = {}
+      end 
+      if !pigeon_list[name][:gender]
+        pigeon_list[name][:gender] = []
+      end 
+      pigeon_list[name][:gender].push(gender)
+    end 
+  end
+  
   # Do all lives.
+   data[:lives].each do |where, pigeon_names|
+    pigeon_names.each do |name|
+      if !pigeon_list[name]
+        pigeon_list[name] = {}
+      end 
+      if !pigeon_list[name][:lives]
+        pigeon_list[name][:lives] = []
+      end 
+      pigeon_list[name][:gender].push(where)
+    end 
+  end
   
   return pigeon_list
 end
