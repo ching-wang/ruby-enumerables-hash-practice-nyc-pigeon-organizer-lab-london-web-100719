9d78135d45a7f497d328eedeff68caada4ccb038
Qing Wang <chingwang.qw@gmail.com>
2019-09-27 21:20:54 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,13 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  # Do all colours.
+  data[:color].each do |attri_value|
+    
+    end
+  
+  
+  # Do all genders.
+  # Do all lives.
+  
+  return pigeon_list
 end
